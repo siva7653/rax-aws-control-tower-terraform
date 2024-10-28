30a54cd8476d38b72d1ce0d996bcad2bcf7b2d8e
soumikrackspace <soumik.mukherjee@rackspace.com>
2024-10-28 09:42:00 +0530

    Deleted Old

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-# AWS Control Tower Controls (sometimes called Guardrails) Outputs
-
-output "ous_id_to_arn_map" {
-  value       = local.ous_id_to_arn_map
-  description = "Map from OU id to OU arn for the whole organization"
-}
--- scp.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_organizations_policy_attachment" "attach_custom_scp" {
-  policy_id = "p-966ekd3w"  # Custom SCP ID
-  target_id = "ou-zl72-rektquu3"  # Non Production OU ID
-}
-
-# resource "aws_organizations_policy_attachment" "attach_custom_scp_security" {
-#   policy_id = "p-1a2b3c4d"  # Another Custom SCP ID
-#   target_id = "ou-zl72-85iyjbk6"  # Security OU ID
-# }
