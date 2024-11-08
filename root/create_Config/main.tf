2a9050169cd30185420a34ebe3b2c60b45e73d9b
siva7653 <siva.vanga@rackspace.com>
2024-11-08 12:08:58 +0530

    Update main.tf

--- root/create_Config/main.tf
+++ root/create_Config/main.tf
@@ -1,6 +1,10 @@
+variable "policy_name" {
+  description = "The name of the AWS Organizations policy"
+  type        = string
+}
 # 1. restricted-ssh (Restrict SSH Access)
 resource "aws_config_config_rule" "restricted_ssh" {
-  name        = "rax-restricted-ssh"
+  name        = var.policy_name
   description = "Ensure SSH access is restricted."
   source {
     owner             = "AWS"
